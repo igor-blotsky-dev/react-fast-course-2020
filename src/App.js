3ea1aeda748c4ccf2aaf8e833af96b14d15d7265
igor.blotskiy <igor.blotskiy@codex-soft.com>
2022-04-02 19:31:29 +0300

    feat: add loader

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import TodoList from "./Todo/TodoList";
 import Context from "./context";
 import AddTodo from "./Todo/AddTodo";
+import Loader from "./Loader";
 
 function App() {
-  const [todos, setTodos] = useState([
-    {id: 1, completed: false, title: "Buy bread"},
-    {id: 2, completed: true, title: "Buy butter"},
-    {id: 3, completed: true, title: "Buy cola"},
-  ]);
+  const [todos, setTodos] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos?_limit=5 ')
+      .then(response => response.json())
+      .then(todos => setTimeout(() => {
+        setTodos(todos)
+        setLoading(false)
+      }, 3000))
+  }, [])
 
   function toggleTodo(id) {
     setTodos(
@@ -38,9 +45,11 @@ function App() {
         <h1>React tutorial</h1>
         <AddTodo onCreate={addTodo}/>
         {
-          todos.length
-            ? <TodoList todos={todos}/>
-            : <p>no todos</p>
+          loading
+            ? <Loader/>
+            : todos.length
+              ? <TodoList todos={todos}/>
+              : <p>no todos</p>
         }
 
 
