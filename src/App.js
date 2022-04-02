c29a4ac48f7038478047a81038168d81b488114e
igor.blotskiy <igor.blotskiy@codex-soft.com>
2022-04-02 15:31:43 +0300

    feat: add list, toggle, remove item

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import TodoList from "./Todo/TodoList";
+import Context from "./context";
 
 function App() {
+  const [todos, setTodos] = useState([
+    {id: 1, completed: false, title: "Buy bread"},
+    {id: 2, completed: true, title: "Buy butter"},
+    {id: 3, completed: true, title: "Buy cola"},
+  ]);
+
+  function toggleTodo(id) {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id !== id) return todo;
+
+        return {...todo, completed: !todo.completed};
+      })
+    );
+  }
+
+  function removeTodo(id) {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Context.Provider value={{toggleTodo, removeTodo}}>
+      <div className="wrapper">
+        <h1>React tutorial</h1>
+
+        <TodoList todos={todos}/>
+      </div>
+    </Context.Provider>
   );
 }
 
---
+++ src/Todo/TodoItem.js
@@ -0,0 +1,55 @@
+import React, {useContext} from "react";
+import PropTypes from "prop-types";
+import Context from "../context";
+
+const styles = {
+  li: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: ".5rem 1rem",
+    border: "1px solid #ccc",
+    borderRadius: "4px",
+    marginBottom: ".5rem",
+  },
+  input: {
+    marginRight: "1rem",
+  },
+};
+
+function TodoItem({todo, index}) {
+  const {removeTodo, toggleTodo} = useContext(Context)
+
+  const classes = [];
+
+  if (todo.completed) {
+    classes.push("done");
+  }
+
+  return (
+    <li style={styles.li}>
+      <label className={classes.join(" ")}>
+        <input
+          type="checkbox"
+          checked={todo.completed}
+          style={styles.input}
+          onChange={() => toggleTodo(todo.id)}
+        />
+        <strong>{index + 1}</strong>
+        &nbsp;
+        {todo.title}
+      </label>
+
+      <button className="rm" onClick={removeTodo.bind(null, todo.id)}>
+        &times;
+      </button>
+    </li>
+  );
+}
+
+TodoItem.propTypes = {
+  todo: PropTypes.object.isRequired,
+  index: PropTypes.number
+};
+
+export default TodoItem;
